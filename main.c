41c7f0d519b2c1dfb237f269b2badd3e2f93b533
abdelrahman <abdelrahman@github.com>
2023-07-26 17:46:16 -0700

    simpel_shell

--- main.c
+++ main.c
@@ -6,14 +6,18 @@
 #define MAX_INPUT_LENGTH 100
 #define MAX_ARGS 10
 
-int main() {
+int main(void)
+{
+
     char input[MAX_INPUT_LENGTH];
     char *args[MAX_ARGS];
 
-    while (1) {
+    while (1)
+    {
         print_prompt();  // Display the prompt
 
-        if (fgets(input, sizeof(input), stdin) == NULL) {
+        if (fgets(input, sizeof(input), stdin) == NULL)
+        {
             // Check for "end of file" condition (Ctrl+D)
             printf("\nExiting the shell.\n");
             break;
@@ -25,7 +29,28 @@ int main() {
         // Split the input into arguments
         int arg_count = split_input(input, args);
 
-        // ... (rest of the main function as before)
+        // Check if the user entered "exit"
+        if (arg_count > 0 && strcmp(args[0], "exit") == 0)
+        {
+            // Free memory allocated for arguments
+            for (int i = 0; i < arg_count; i++)
+            {
+                free(args[i]);
+            }
+            printf("\n");
+            break;
+        }
+        else if (arg_count > 0)
+        {
+            // ... (rest of the execution logic for commands)
+            execute_command(args);
+        }
+
+        // Free memory allocated for arguments
+        for (int i = 0; i < arg_count; i++)
+        {
+            free(args[i]);
+        }
     }
 
     return 0;
